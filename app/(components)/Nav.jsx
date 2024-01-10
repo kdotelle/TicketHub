987712e4d8803676e1ffd4570dda51d632f430e9
Kristen <lloyd.kristen91@gmail.com>
2024-01-09 21:14:11 -0500

    added components

---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTicket, faHome } from "@fortawesome/free-solid-svg-icons";
+
+const Nav = () => {
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-default-text">kristen@email.com</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
